1092e5f77168abafaf7573afa0a8dd6e7088f4d2
myoma <102959354+GyeonghwanCheon@users.noreply.github.com>
2024-05-18 17:11:07 +0900

    [level 0] Title: 문자열 섞기, Time: 13.91 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -4,8 +4,10 @@ class Solution {
         
         for(int i=0; i<str1.length(); i++) {
         
-            answer += String.valueOf(str1.charAt(i))+str2.charAt(i);
+            answer += "" + str1.charAt(i)+str2.charAt(i);
         }
+        //String.valueOf를 사용하여 값을 String으로 형변환를 하나시켜도 됨.
+        //""을 문자열 앞에 두고 더하면 str1이 문자열로 더해져 뒤에 str2도 문자열로 더해짐.
         
         return answer;
     }
