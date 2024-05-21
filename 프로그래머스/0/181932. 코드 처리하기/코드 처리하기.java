536e45dd459522b2425b151e0712f432c7a0dfcc
myoma <102959354+GyeonghwanCheon@users.noreply.github.com>
2024-05-21 20:18:15 +0900

    [level 0] Title: 코드 처리하기, Time: 0.02 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        // mode = 0 일 땐 짝수 index 추가
+        // mode = 1 일 땐 홀수 index 추가
+        int mode = 0;
+        String ret = "";
+        
+        for(int i = 0; i<code.length(); i++) {
+            if(code.charAt(i) == '1'){
+              mode = 1 - mode;
+            }
+            else if(i % 2 == mode) {
+                answer += code.charAt(i);
+            }
+        }
+        
+        
+        
+        return "".equals(answer) ? "EMPTY" : answer;
+    }
+}
