c32c5a6b79b55e4e2e37f27626f3afa343682b92
myoma <102959354+GyeonghwanCheon@users.noreply.github.com>
2024-05-24 22:54:02 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -8,10 +8,7 @@ public class Solution {
         for(int i=0; i< s.length(); i++){
             answer += s.charAt(i) -'0';
         }
-        
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+    
 
         return answer;
     }
