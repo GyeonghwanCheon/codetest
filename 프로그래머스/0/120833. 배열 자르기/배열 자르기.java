d99c11a9f22296b7ffcfdf951639835c74917cb3
myoma <102959354+GyeonghwanCheon@users.noreply.github.com>
2024-05-27 23:19:23 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[numbers.length];
+        
+        return answer = Arrays.copyOfRange(numbers, num1, num2 + 1);
+    }
+}
