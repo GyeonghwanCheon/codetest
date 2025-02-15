b46573d74f2440f87cd7bdc7d72fd1355ab12304
myoma <102959354+GyeonghwanCheon@users.noreply.github.com>
2025-02-15 17:12:20 +0900

    [level 1] Title: 명예의 전당 (1), Time: 16.68 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        
+        List<Integer> rank = new ArrayList<>();
+        
+        for(int i = 0; i < score.length; i++) {
+            rank.add(score[i]);
+            Collections.sort(rank,Collections.reverseOrder());
+            
+            if(rank.size() <= k) {
+                answer[i] = rank.get(i);
+            } else {
+                answer[i] = rank.get(k-1);
+            }
+        }
+        
+        return answer;
+    }
+}
