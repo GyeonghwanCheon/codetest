972e906d4d422b84aaf856fb1b272aa91a5d26ad
myoma <102959354+GyeonghwanCheon@users.noreply.github.com>
2026-02-09 21:31:58 +0900

    [level 0] Title: 주사위의 개수, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] box, int n) {
+        int answer = 0;
+        
+        for(int i = 0; i<box.length; i++) {
+            box[i] = box[i]/n;
+        }
+        
+        return box[0] * box[1] * box[2];
+    }
+}
