c9404f0f9701e0efbee4759153de169dda6dcf5e
myoma <102959354+GyeonghwanCheon@users.noreply.github.com>
2026-02-15 15:56:21 +0900

    [level 0] Title: 배열 만들기 3, Time: 2.46 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        int length1 = intervals[0][1] - intervals[0][0] + 1;
+        int length2 = intervals[1][1] - intervals[1][0] + 1;
+        
+        int[] answer = new int[length1 + length2];
+        
+        int idx = 0;
+        
+        for(int i = intervals[0][0]; i<=intervals[0][1]; i++) {
+            answer[idx++] = arr[i];
+        }
+        
+        for(int i = intervals[1][0]; i<= intervals[1][1]; i++) {
+            answer[idx++] = arr[i];
+        }
+    
+        return answer;
+    }
+}
