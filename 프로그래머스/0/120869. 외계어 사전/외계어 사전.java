3c7579654d2b56e1dbe1153295a4f9c5cb15c91e
myoma <102959354+GyeonghwanCheon@users.noreply.github.com>
2026-05-26 22:30:35 +0900

    [level 0] Title: 외계어 사전, Time: 0.07 ms, Memory: 60.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+        int answer = 0;
+        
+        for(int i = 0; i < dic.length; i++) {
+            int count = 0;
+            
+            for(int j = 0; j < spell.length; j++) {
+                if(dic[i].contains(spell[j])) {
+                    count ++;
+                }
+            }
+            
+            if(spell.length == count) {
+                return 1;
+            }
+        }
+        return 2;
+    }
+}
