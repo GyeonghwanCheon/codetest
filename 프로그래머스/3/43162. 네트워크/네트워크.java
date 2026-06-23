bf0650a38cb12958d703dd29dc8b9c679c3e50eb
myoma <102959354+GyeonghwanCheon@users.noreply.github.com>
2026-06-23 22:46:04 +0900

    [level 3] Title: 네트워크, Time: 0.14 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,27 @@
+class Solution {
+    boolean[] visited;
+    
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new boolean[n];
+        
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                dfs(i, n, computers);
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public void dfs(int index, int n, int[][] computers) {
+        visited[index] = true;
+        
+        for(int i = 0; i < n; i++) {
+            if(computers[index][i] == 1 && !visited[i]) {
+                dfs(i, n, computers);
+            }
+        }
+    }
+}
