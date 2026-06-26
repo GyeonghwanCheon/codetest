d4e9124656abe3fc18802df55a17b2ac0f2e45b7
myoma <102959354+GyeonghwanCheon@users.noreply.github.com>
2026-06-26 21:37:17 +0900

    [level 3] Title: 정수 삼각형, Time: 5.50 ms, Memory: 63.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int[][] triangle) {
+        
+        for(int i = 1; i < triangle.length; i++) {
+            for(int j = 0; j < triangle[i].length; j++) {
+                
+                if(j == 0) {
+                    triangle[i][j] += triangle[i - 1][j];
+                } else if(j == triangle[i].length - 1) {
+                    triangle[i][j] += triangle[i - 1][j - 1];
+                } else {
+                    triangle[i][j] += Math.max(triangle[i - 1][j - 1], triangle[i - 1][j]);
+                }
+            }
+        }
+        
+        int answer = 0;
+        int last = triangle.length - 1;
+        
+        for(int i = 0; i < triangle[last].length; i++) {
+            answer = Math.max(answer, triangle[last][i]);
+        }
+        
+        return answer;
+    }
+}
